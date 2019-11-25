8f2fd537a8b413bcb6d3696c62fe0874bf41bb74
Angelo Garcia <pvzguymanthing@gmail.com>
2019-11-25 20:19:01 +0400

    I finished the BattleSystem, DanceTeam, and FightManager.  In FightManager, I simply added BattleLog to display the winner and if it is a draw. In DanceTeam, I added a dancer to the list allDancers and activeDancers in the AddNewDancer Function. I also added the dancer with the DanceTeam class. Afterwards in the RemoveFromActive function I remove the activeDancers and set the mojoRemaining to 0. In BattleSystem under DoRound, I called the RequestFight and input the activeDancers currently which is randomized, and after that I wrote an if statement to check if activeDancers is set to 0 in a team. This determines who is the winner. Under the FightOver function I wrote an if statement to check if the outcome does not equel to 0. If so then it gives the win effect to the winner and remove the losing dancer in the activeDancers list. Oh, and I also changed the number of dancers in each team to 3 instead of 6.

---
+++ Assets/Scripts/FightManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Handles the outcome of a dance off between 2 dancers, determines the strength of the victory form -1 to 1
+/// 
+/// TODO:
+///     Handle GameEvents.OnFightRequested, resolve based on stats and respond with GameEvents.FightCompleted
+///         This will require a winner and defeated in the fight to be determined.
+///         This may be where characters are set as selected when they are in a dance off and when they leave the dance off
+///         This may also be where you use the BattleLog to output the status of fights
+///     This may also be where characters suffer mojo (hp) loss when they are defeated
+/// </summary>
+public class FightManager : MonoBehaviour
+{
+    public Color drawCol = Color.gray;
+
+    public float fightAnimTime = 2;
+
+    private void OnEnable()
+    {
+        GameEvents.OnFightRequested += Fight;
+    }
+
+    private void OnDisable()
+    {
+        GameEvents.OnFightRequested -= Fight;
+    }
+
+    public void Fight(FightEventData data)
+    {
+        StartCoroutine(Attack(data.lhs, data.rhs));
+    }
+
+    IEnumerator Attack(Character lhs, Character rhs)
+    {
+        lhs.isSelected = true;
+        rhs.isSelected = true;
+        lhs.GetComponent<AnimationController>().Dance();
+        rhs.GetComponent<AnimationController>().Dance();
+
+        yield return new WaitForSeconds(fightAnimTime);
+
+        float outcome = 0;
+        //defaulting to draw 
+        Character winner = lhs, defeated = rhs;
+        //Debug.LogWarning("Attack called, needs to use character stats to determine winner with win strength from 1 to -1. This can most likely be ported from previous brief work.");
+        outcome = (rhs.rhythm / rhs.style + rhs.luck) - (lhs.rhythm / lhs.style + lhs.luck);
+        outcome = Mathf.Clamp(outcome, -1f, 1f);
+        if (outcome > 0)
+        {
+            winner = rhs;
+            defeated = lhs;
+        }
+        else if(outcome < 0)
+        {
+            winner = lhs;
+            defeated = rhs;
+        }
+        else
+        {
+            Debug.Log("It's a draw");
+        }
+
+        if (outcome != 0)
+        {
+            BattleLog.Log(new DefaultLogMessage(winner.charName.GetFullCharacterName() + " is the Winnner!", winner.myTeam.teamColor));
+        }
+        else
+        {
+            BattleLog.Log(new DefaultLogMessage(lhs.charName.GetFullCharacterName() + " ties with " + rhs.charName.GetFullCharacterName(),drawCol));
+        }
+
+        var results = new FightResultData(winner, defeated, outcome);
+
+        lhs.isSelected = false;
+        rhs.isSelected = false;
+        GameEvents.FightCompleted(results);
+
+        yield return null;
+    }
+}
